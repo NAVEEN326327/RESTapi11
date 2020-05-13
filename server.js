f0fd8294232d97de475d2ee5f58659f1464570d7
NAVEEN326327 <nks326327@gmail.com>
2020-05-13 16:40:30 +0530

    complete

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/user'
+const uri = process.env.ATLAS_URI;
+// const uri = process.env.MONGODB_URI || 'ATLAS_URI'
 
 mongoose.connect( uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false}
     );
